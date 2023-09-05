8357606b13a99064df5f5ea9479ec86f90323976
Anatoly Mezenov <xxgladgodxx@mail.ru>
2023-09-06 00:41:27 +0500

    add parser and loginfo update reader

---
+++ lab-1/loginfo.py
@@ -0,0 +1,10 @@
+#created by GlaDGoD
+
+class LogInfo:
+    def __init__(self, ip: str, date_time: str, request: str, code: str, response: str, user_id: str):
+        self.ip = ip
+        self.date_time = date_time
+        self.request = request
+        self.code = code
+        self.response = response
+        self.user_id = user_id
--- lab-1/reader.py
+++ lab-1/reader.py
@@ -1,8 +1,23 @@
 from typing import List, TextIO
-
-
+from logparser import parser
+from loginfo import LogInfo
 def load_data(path: str) -> List[str]:
     file: TextIO = open(file=path, mode="r")
     content: List[str] = file.readlines()
     # TODO to LIST[DTO]
     return content
+
+loginfoDatabase = []
+for log in load_data("../access.log"):
+    try:
+        parse = parser(log)
+    except:
+        continue
+    loginfoDatabase.append(LogInfo(
+        parse["ip"],
+        parse["date"],
+        parse["request"],
+        parse["code"],
+        parse["response"],
+        parse["id"]
+    ))
