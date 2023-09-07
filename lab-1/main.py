5df5931714984b38690eeaa44fab59c4483330fa
AlexCawl <babushkinsusu@gmail.com>
2023-09-07 21:41:34 +0500

    fix: typings added

--- lab-1/main.py
+++ lab-1/main.py
@@ -1,9 +1,11 @@
+from typing import Tuple, List
+
 from mapper import map_logs_to_dataframe
 from reader import load_logs_from_file
 import LogDTO
 
 if __name__ == '__main__':
-    data: tuple[list[LogDTO], int] = load_logs_from_file("../data/access.log")
+    data: Tuple[List[LogDTO], int] = load_logs_from_file("../data/access.log")
     print("Accessed data")
     df = map_logs_to_dataframe(data[0])
     print(df)
