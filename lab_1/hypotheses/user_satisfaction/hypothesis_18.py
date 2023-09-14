a92b34833def821e69de4cd0009e4a70a80ed138
astronely <nikborowik200394@gmail.com>
2023-09-14 23:57:59 +0500

    update: hypothesis_4 implementation

--- lab_1/hypotheses/user_satisfaction/hypothesis_18.py
+++ lab_1/hypotheses/user_satisfaction/hypothesis_18.py
@@ -1,10 +1,11 @@
-from typing import Tuple
+from typing import Tuple, List, Dict
 
 import pandas as pd
+import datetime
 
 from lab_1.util.Hypothesis import Hypothesis
 from lab_1.util.decorators import measure_execution_time
-
+from lab_1.util.constants import *
 
 # №18
 # Вопрос: Какова удовлетворенность клиентов от взаимодействия с сайтом?
@@ -17,5 +18,33 @@ def compute(dataframe: pd.DataFrame, comparable_value: float) -> Tuple[str, str]
         h1="Среднее время браузинга товаров на сайте не меньше, чем {val}",
         condition=lambda x: x > comparable_value
     )
-    value: float = 2  # computed from dataframe
+    users: Dict[str, List[str]] = dict()
+    users_count: int = 0
+
+    for index in range(len(dataframe)):
+        row: pd.Series = dataframe.loc[index]
+        user_id: str = str(row[ID])
+        _datetime: str = str(row[DATETIME])[:19]
+        if users.get(user_id) is None:
+            users.update({user_id: [_datetime]})
+            users_count += 1
+        else:
+            list_to_update: List[str] = users[user_id]
+            if len(users[user_id]) == 1:
+                list_to_update.append(_datetime)
+                users.update({user_id: list_to_update})
+            else:
+                list_to_update[1] = _datetime
+                users.update({user_id: list_to_update})
+
+    total_diff: float = 0
+    for key in users.keys():
+        if len(users[key]) > 1:
+            first_request: datetime.datetime = datetime.datetime.strptime(users[key][0], "%d/%b/%Y:%H:%M:%S")
+            last_request: datetime.datetime = datetime.datetime.strptime(users[key][1], "%d/%b/%Y:%H:%M:%S")
+
+            diff: float = (last_request-first_request).total_seconds()/60
+            total_diff += diff
+
+    value: float = total_diff/users_count
     return hypothesis.compute(value), f"{value}"
--- lab_1/hypotheses/user_satisfaction/hypothesis_19.py
+++ lab_1/hypotheses/user_satisfaction/hypothesis_19.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from lab_1.util.Hypothesis import Hypothesis
 from lab_1.util.decorators import measure_execution_time
-
+from lab_1.util.constants import *
 
 # №19
 # Вопрос: Какова удовлетворенность клиентов от взаимодействия с сайтом?
@@ -17,5 +17,37 @@ def compute(dataframe: pd.DataFrame, comparable_value: float) -> Tuple[str, str]
         h1="при формировании своей продуктовой корзины, покупатель с большей степенью воспользуется ПОИСКОМ, нежели КАТАЛОГОМ",
         condition=lambda x: x > comparable_value
     )
-    value: float = 2  # computed from dataframe
-    return hypothesis.compute(value), f"{value}"
+
+    def user_last_request(_dataframe: pd.DataFrame, _index: int, _user_id: str) -> int:
+        for i in range(_index, -1, -1):
+            _row: pd.Series = _dataframe.loc[i]
+            if str(_row[ID]) == _user_id:
+                _url: str = str(_row[URL])
+                if _url.startswith(CATALOG):
+                    return 0
+                elif _url.startswith(SEARCH):
+                    return 1
+        return -1
+
+    catalog_count: int = 0
+    search_count: int = 0
+
+    for index in range(len(dataframe)):
+        row: pd.Series = dataframe.loc[index]
+        user_id: str = str(row[ID])
+        url: str = str(row[URL])
+
+        if url.startswith(ADDBASKET):
+            last_request: int = user_last_request(dataframe, index, user_id)
+            if last_request == 0:
+                catalog_count += 1
+            elif last_request == 1:
+                search_count += 1
+
+    hypothesis: Hypothesis = Hypothesis(
+        h0="при формировании своей продуктовой корзины, покупатель с большей степенью воспользуется КАТАЛОГОМ, нежели ПОИСКОМ",
+        h1="при формировании своей продуктовой корзины, покупатель с большей степенью воспользуется ПОИСКОМ, нежели КАТАЛОГОМ",
+        condition=lambda x: x > search_count
+    )
+
+    return hypothesis.compute(catalog_count), f"{catalog_count} | {search_count}"
