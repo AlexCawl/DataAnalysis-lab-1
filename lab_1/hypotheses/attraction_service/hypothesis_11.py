f75883f029326bfee751a81d5b9f5f3ce66e8da3
Oplayer1337 <44051933+Oplayer1337@users.noreply.github.com>
2023-09-15 19:12:35 +0500

    feat: added implementation of hypothesis_11

--- lab_1/hypotheses/attraction_service/hypothesis_11.py
+++ lab_1/hypotheses/attraction_service/hypothesis_11.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Dict, Callable
+from typing import Tuple, Dict, Callable, Set
 
 import pandas as pd
 
@@ -15,8 +15,20 @@ def compute(dataframe: pd.DataFrame, comparable_value: float) -> Tuple[str, str]
     h0: str = "Коэффициент становление клиентом из посетителя больше, чем {VAL}"
     h1: str = "Коэффициент становление клиентом из посетителя не больше, чем {VAL}"
     condition: Callable[[int], bool] = lambda t: t > comparable_value
-    # /// реализация гипотезы \\\
-    result: float = 2  # computed from dataframe
+
+    users: Set[str] = set()
+    customers: Set[str] = set()
+
+    for index in range(len(dataframe)):
+        row: pd.Series = dataframe.loc[index]
+        user_id: str = str(row[ID])
+        url: str = str(row[URL])
+
+        if url.startswith(ADDBASKET):
+            customers.add(user_id)
+        users.add(user_id)
+    # TODO поправить вывод гипотезы
+    result: float = len(customers) / len(users)  # computed from dataframe
     return (
         h0.format(VAL=result) if condition(result) else h1.format(VAL=result),
         f""
