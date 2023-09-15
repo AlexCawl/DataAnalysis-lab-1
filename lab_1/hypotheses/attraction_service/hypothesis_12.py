ae7e5c72491e13094609bf014510cf58e1744cf1
WineMick <mdedushkin10@gmail.com>
2023-09-15 16:26:49 +0500

    fix: refactor hypothesis_12.py

--- lab_1/hypotheses/attraction_service/hypothesis_12.py
+++ lab_1/hypotheses/attraction_service/hypothesis_12.py
@@ -1,10 +1,9 @@
-from typing import Tuple
+from typing import Tuple, Callable, Dict, List
 
 import pandas as pd
 
-from lab_1.util.Hypothesis import Hypothesis
-from lab_1.util.decorators import measure_execution_time
 from lab_1.util.constants import *
+from lab_1.util.decorators import measure_execution_time
 
 
 # №12
@@ -13,31 +12,31 @@ from lab_1.util.constants import *
 
 @measure_execution_time
 def compute(dataframe: pd.DataFrame, comparable_value: float) -> Tuple[str, str]:
-    hypothesis: Hypothesis = Hypothesis(
-        h0="Среднее число посетителей за день больше, чем {val}",
-        h1="Среднее число посетителей за день не больше, чем {val}",
-        condition=lambda x: x > comparable_value
-    )
-
-    visitors_per_day: dict[str: list[str]] = dict()
-    counter_unique_visitors: int = 0
+    h0: str = f"Среднее число посетителей за день больше, чем {comparable_value:.2f}"
+    h1: str = f"Среднее число посетителей за день не больше, чем {comparable_value:.2f}"
+    condition: Callable[[int], bool] = lambda x: x > comparable_value
+    visitors: Dict[str, List[str]] = dict()
+    unique_visitors: int = 0
 
     for index in range(len(dataframe)):
         row: pd.Series = dataframe.loc[index]
         user_id: str = str(row[ID])
         _date: str = str(row[DATETIME])[:10]
 
-        if _date not in visitors_per_day.keys():
-            visitors_per_day.update({_date: []})
+        if _date not in visitors.keys():
+            visitors.update({_date: []})
 
-        if user_id not in visitors_per_day[_date]:
-            visitors_per_day[_date].append(user_id)
+        if user_id not in visitors[_date]:
+            visitors[_date].append(user_id)
 
     overall_sum: int = 0
 
-    for amount_of_users in visitors_per_day.values():
+    for amount_of_users in visitors.values():
         sum_for_day: int = len(amount_of_users)
         overall_sum += sum_for_day
 
-    value: float = overall_sum / len(visitors_per_day)  # computed from dataframe
-    return hypothesis.compute(value), f"{value}"
+    result: float = overall_sum / len(visitors)
+    return (
+        h0 if condition(result) else h1,
+        f"overall_sum={overall_sum}; visitors_count={len(visitors)}; result={result}"
+    )
