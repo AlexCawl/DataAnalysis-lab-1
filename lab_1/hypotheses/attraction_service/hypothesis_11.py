286ef3284972cf8683ac41746a114cb7c572a55a
Oplayer1337 <44051933+Oplayer1337@users.noreply.github.com>
2023-09-15 18:43:31 +0500

    fix: changed template for hypothesis_11.py, hypothesis_13.py, hypothesis_14.py, hypothesis_15.py

--- lab_1/hypotheses/attraction_service/hypothesis_11.py
+++ lab_1/hypotheses/attraction_service/hypothesis_11.py
@@ -1,9 +1,9 @@
-from typing import Tuple
+from typing import Tuple, Dict, Callable
 
 import pandas as pd
 
-from lab_1.util.Hypothesis import Hypothesis
 from lab_1.util.decorators import measure_execution_time
+from lab_1.util.constants import *
 
 
 # №11
@@ -12,10 +12,12 @@ from lab_1.util.decorators import measure_execution_time
 
 @measure_execution_time
 def compute(dataframe: pd.DataFrame, comparable_value: float) -> Tuple[str, str]:
-    hypothesis: Hypothesis = Hypothesis(
-        h0="Коэффициент становление клиентом из посетителя больше, чем {val}",
-        h1="Коэффициент становление клиентом из посетителя не больше, чем {val}",
-        condition=lambda x: x > comparable_value
+    h0: str = "Коэффициент становление клиентом из посетителя больше, чем {VAL}"
+    h1: str = "Коэффициент становление клиентом из посетителя не больше, чем {VAL}"
+    condition: Callable[[int], bool] = lambda t: t > comparable_value
+    # /// реализация гипотезы \\\
+    result: float = 2  # computed from dataframe
+    return (
+        h0.format(VAL=result) if condition(result) else h1.format(VAL=result),
+        f""
     )
-    value: float = 2  # computed from dataframe
-    return hypothesis.compute(value), f"{value}"
