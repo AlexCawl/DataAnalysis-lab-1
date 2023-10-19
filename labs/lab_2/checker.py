5ac48de19380afc74bf6ca9ce77f8c4e6b70926a
astronely <nikborowik200394@gmail.com>
2023-10-19 21:50:07 +0500

    update: add decision tree implementation

--- labs/lab_2/checker.py
+++ labs/lab_2/checker.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from labs.lab_2.lda_hypothesis.LDAModel import LDAModel
 from labs.lab_2.svm_hypothesis.SVMModel import SVMModel
+from labs.lab_2.dtc_hypothesis.DTModel import DTModel
 from labs.lab_2.util.constants import RESULT
 from labs.lab_2.util.pandas_util import split_dataframe_into_samples
 from labs.util.benchmarking.measuring import measure_execution_time
@@ -18,7 +19,7 @@ def check_hypotheses(dataframe: pd.DataFrame):
 
     output_path = mk_dir_abs_from_local(f"{DATA_OUTPUT_FOLDER}/lab2")
 
-    models = [LDAModel, SVMModel]
+    models = [LDAModel, SVMModel, DTModel]
     for model in models:
         model = model()
         model.train(X_VAL_TRAIN_SAMPLE, Y_VAL_TRAIN_SAMPLE)
