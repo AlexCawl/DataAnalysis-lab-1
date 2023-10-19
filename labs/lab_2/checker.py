da81ba19589957a03f1adbe81fa40c23b5533fe5
Oplayer1337 <44051933+Oplayer1337@users.noreply.github.com>
2023-10-19 22:32:03 +0500

    add: added implementation of GNBModel

--- labs/lab_2/checker.py
+++ labs/lab_2/checker.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 from labs.lab_2.lda_hypothesis.LDAModel import LDAModel
 from labs.lab_2.svm_hypothesis.SVMModel import SVMModel
+from labs.lab_2.knn_hypothesis.KNNModel import KNNModel
+from labs.lab_2.gnb_hypothesis.GNBModel import GNBModel
 from labs.lab_2.util.constants import RESULT
 from labs.lab_2.util.pandas_util import split_dataframe_into_samples
 from labs.util.benchmarking.measuring import measure_execution_time
@@ -17,8 +19,7 @@ def check_hypotheses(dataframe: pd.DataFrame):
     ) = split_dataframe_into_samples(dataframe, RESULT)
 
     output_path = mk_dir_abs_from_local(f"{DATA_OUTPUT_FOLDER}/lab2")
-
-    models = [LDAModel, SVMModel]
+    models = [LDAModel, SVMModel, KNNModel, GNBModel]
     for model in models:
         model = model()
         model.train(X_VAL_TRAIN_SAMPLE, Y_VAL_TRAIN_SAMPLE)
---
+++ labs/lab_2/gnb_hypothesis/GNBModel.py
@@ -0,0 +1,61 @@
+from typing import Any, Dict
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.metrics import accuracy_score, classification_report, ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import RepeatedStratifiedKFold, GridSearchCV
+from sklearn.naive_bayes import GaussianNB
+
+from labs.lab_2.util.ClassificationModelApi import ClassificationModelApi
+from labs.lab_2.util.constants import CLASSES
+
+
+class GNBModel(ClassificationModelApi):
+    __is_trained: bool
+    __grid: Dict[str, Any]
+    __search: GridSearchCV
+    __results: LinearDiscriminantAnalysis
+    __cv: RepeatedStratifiedKFold
+
+    __score: float
+    __matrix: Any
+    __report: str | dict
+
+    def __init__(self) -> None:
+        self.__is_trained = False
+        self.__grid = {
+            'priors': [None],
+            'var_smoothing': np.arange(0.000000001, 0.00000001, 0.000000001)
+        }
+
+        self.__cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+
+    def get_info(self) -> str:
+        return f"<<<   {self.__class__.__name__}   >>>\n" + \
+            f"Mean Accuracy Trained: {round(self.__results.best_score_ * 100, 2)}%\n" + \
+            f"Mean Accuracy Tested: {round(self.__score * 100, 2)}%\n" + \
+            f"Configuration: {self.__results.best_params_}\n" + \
+            f"Confusion Matrix:\n{self.__matrix}\n" + \
+            f"Classification report:\n{self.__report}"
+
+    def train(self, x: pd.DataFrame, y: pd.DataFrame) -> None:
+        self.__is_trained = True
+        self.__search = GridSearchCV(GaussianNB(), self.__grid, scoring="accuracy", refit=True, cv=self.__cv)
+        self.__results = self.__search.fit(x, y)
+
+    def test(self, x: pd.DataFrame, y: pd.DataFrame, path: str) -> None:
+        if self.__is_trained:
+            prediction = self.__search.best_estimator_.predict(x)
+            self.__score = accuracy_score(y, prediction)
+            self.__matrix = confusion_matrix(y, prediction)
+            self.__report = classification_report(y, prediction)
+
+            plt.figure(figsize=(15, 15))
+            ConfusionMatrixDisplay.from_estimator(self.__search.best_estimator_, x, y, display_labels=CLASSES)
+            plt.savefig(f"{path}/{self.__class__.__name__}-matrix.png")
+            plt.clf()
+        else:
+            raise Exception("Not trained already!")
