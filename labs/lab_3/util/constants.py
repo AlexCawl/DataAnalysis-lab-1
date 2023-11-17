074f8047618abdafea7bf7aae87c64bae3922c48
AlexCawl <babushkinsusu@gmail.com>
2023-11-17 22:01:37 +0500

    feat: exploring linear regression

--- labs/lab_3/util/constants.py
+++ labs/lab_3/util/constants.py
@@ -1,2 +1,17 @@
+from typing import List
+
 PATH_TRAIN: str = "../../../data-in/lab_3/Training/"
 PATH_TEST: str = "../../../data-in/lab_3/Testing/TestSet/"
+
+TRAIN_FILES: List[str] = [
+    'Features_Variant_1.csv',
+    'Features_Variant_2.csv',
+    'Features_Variant_3.csv',
+    'Features_Variant_4.csv',
+    'Features_Variant_5.csv'
+]
+
+TEST_FILES: List[str] = ['Test_Case_1.csv', 'Test_Case_2.csv', 'Test_Case_3.csv',
+                         'Test_Case_4.csv', 'Test_Case_5.csv', 'Test_Case_5.csv',
+                         'Test_Case_6.csv', 'Test_Case_7.csv', 'Test_Case_8.csv',
+                         'Test_Case_9.csv', 'Test_Case_10.csv']
