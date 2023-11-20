3cf6b2566f59f1adb69af9f6e8a4d14dadde8b6b
astronely <nikborowik200394@gmail.com>
2023-11-20 13:01:54 +0500

    update: add decision tree implementation

--- labs/lab_3/checker.py
+++ labs/lab_3/checker.py
@@ -1,6 +1,56 @@
+import glob
+from typing import Callable, Any, Tuple
+
+import pandas as pd
+
+from labs.lab_3.decision_tree_regression.DecisionTreeRegressionModel import DTRModel
 from labs.util.benchmarking.measuring import measure_execution_time
+from labs.util.file_processing.configuration import DATA_OUTPUT_FOLDER
+from labs.util.file_processing.extensions import mk_dir_abs_from_local
+
+
+@measure_execution_time
+def check_model(model: Callable[[], Any], feature_df_train, target_df_train, feature_df_test, target_df_test: pd.DataFrame,
+                output_path: str, balance: bool = None) -> None:
+
+    model = model()
+    model.train(feature_df_train, target_df_train)
+
+    if balance is not None:
+        model.test(feature_df_test, target_df_test, output_path, balance)
+    else:
+        model.test(feature_df_test, target_df_test, output_path)
+
+    print(model.get_info())
+
+
+def generate_df(train_path, test_path) -> Tuple:
+    train_files = glob.glob(train_path + "/*.csv")
+    test_files = glob.glob(test_path + "/*.csv")
+    temp_train_dfs, temp_test_dfs = [], []
+
+    for file in train_files:
+        temp_train_dfs.append(pd.read_csv(file))
+
+    for file in test_files:
+        temp_test_dfs.append(pd.read_csv(file))
+
+    train_df: pd.DataFrame = pd.concat(temp_train_dfs, ignore_index=True)
+    test_df: pd.DataFrame = pd.concat(temp_test_dfs, ignore_index=True)
+
+    target_df_train: pd.DataFrame = train_df['Target Variable']
+    feature_df_train: pd.DataFrame = train_df.drop(columns='Target Variable')
+
+    target_df_test: pd.DataFrame = test_df['Target Variable']
+    feature_df_test: pd.DataFrame = test_df.drop(columns='Target Variable')
+
+    return feature_df_train, target_df_train, feature_df_test, target_df_test
 
 
 @measure_execution_time
 def check_hypotheses(train_path: str, test_path: str):
-    pass
+    output_path = mk_dir_abs_from_local(f"{DATA_OUTPUT_FOLDER}/lab3")
+    models = [DTRModel]
+    feature_df_train, target_df_train, feature_df_test, target_df_test = generate_df(train_path, test_path)
+    for model in models:
+        check_model(model, feature_df_train, target_df_train, feature_df_test, target_df_test, output_path)
--- labs/lab_3/decision_tree_regression/DecisionTreeRegressionModel.py
+++ labs/lab_3/decision_tree_regression/DecisionTreeRegressionModel.py
@@ -10,56 +10,60 @@ from sklearn.tree import DecisionTreeRegressor
 from labs.lab_3.util.RegressionModelApi import RegressionModelApi
 
 
-class DecisionTreeRegressionModel(RegressionModelApi):
+class DTRModel(RegressionModelApi):
     __is_trained: bool
     __grid: Dict[str, Any]
     __search: GridSearchCV
     __cv: RepeatedStratifiedKFold
-    __results = None
-    __score: float
-    __matrix: Any
+    # __results = None
     __report: str | dict
 
     def __init__(self):
+        self.__results = None
         self._RMSE = None
         self._MSE = None
         self._MAE = None
-        self__is_trained = False
-        self.__grid = {'criterion': ['friedman_mse', 'squared_error'],
+        self.__is_trained = False
+        self.__grid = {'criterion': ['friedman_mse'],
                        'max_depth': [i for i in range(5, 20, 2)],
                        'max_leaf_nodes': [i for i in range(20, 60, 10)],
                        'min_samples_split': [i for i in range(20, 80, 10)]}
+        # self.__grid = {'criterion': ['friedman_mse'],
+        #                'max_depth': [10],
+        #                'max_leaf_nodes': [40],
+        #                'min_samples_split': [70]}
 
         self.__cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 
     def get_info(self) -> str:
-        pass
+        return f"<<<   {self.__class__.__name__}   >>>\n" + \
+            f"R-Squared trained: {round(self.__results.best_score_ * 100, 2)}%\n" + \
+            f"MAE Tested: {self._MAE}%\n" + \
+            f"MSE Tested: {self._MSE}%\n" + \
+            f"RMSE Tested: {self._RMSE}%\n" + \
+            f"Configuration: {self.__results.best_params_}\n"
 
-    def train(self, *, x_train: pd.DataFrame, y_train: pd.DataFrame, path: str = "") -> None:
+    def train(self, x_train: pd.DataFrame, y_train: pd.DataFrame) -> None:
         self.__is_trained = True
-        self.__search = GridSearchCV(DecisionTreeRegressor(), self.__grid, scoring="accuracy", refit=True,
-                                     cv=self.__cv)
-        self.__results = self.__search.fit(x_train, y_train)
+        self.__search = GridSearchCV(DecisionTreeRegressor(), self.__grid, cv=6)
+        self.__results = self.__search.fit(X=x_train, y=y_train)
 
-    def test(self, *, x_test: pd.DataFrame, y_test: pd.DataFrame, path: str = "") -> None:
+    def test(self, x_test: pd.DataFrame, y_test: pd.DataFrame, path: str = "") -> None:
         if self.__is_trained:
             prediction = self.__search.best_estimator_.predict(x_test)
             self._MAE = metrics.mean_absolute_error(y_test, prediction)
             self._MSE = metrics.mean_absolute_error(y_test, prediction)
             self._RMSE = np.sqrt(self._MSE)
 
-            print('MAE:', self._MAE)
-            print('MSE:', self._MSE)
-            print('RMSE:', self._RMSE)
-
             plt.scatter(y_test, prediction)
+            plt.savefig(f"{path}/{self.__class__.__name__}-DTR1.png")
 
             fig, ax = plt.subplots()
             ax.scatter(y_test, y_test - prediction)
             ax.axhline(lw=2, color='black')
             ax.set_xlabel('Observed')
             ax.set_ylabel('Residual')
-            plt.show()
+            plt.savefig(f"{path}/{self.__class__.__name__}-DTR2.png")
 
         else:
             raise Exception("Not trained already!")
