033c5046965fd7f4510dfa3897a4a7b88f952228
astronely <nikborowik200394@gmail.com>
2023-11-24 13:13:56 +0500

    update: add decision tree implementation

--- labs/lab_3/random_forest_regression/RandomForestRegressionModel.py
+++ labs/lab_3/random_forest_regression/RandomForestRegressionModel.py
@@ -4,13 +4,12 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn import metrics
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import RepeatedStratifiedKFold, GridSearchCV
-from sklearn.tree import DecisionTreeRegressor
-
 from labs.lab_3.util.RegressionModelApi import RegressionModelApi
 
 
-class DTRModel(RegressionModelApi):
+class RFModel(RegressionModelApi):
     # Model state [Trained - True, otherwise - False]
     __state: bool
 
@@ -21,7 +20,7 @@ class DTRModel(RegressionModelApi):
     __search: GridSearchCV
 
     # Cross-Validation (same for all models)
-    __cv: Any = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=36851234)
+    __cv: Any = RepeatedStratifiedKFold(n_splits=8, n_repeats=1, random_state=36851234)
 
     # Overall report (used to describe model state after training & testing)
     __report: Dict[str, str]
@@ -30,12 +29,14 @@ class DTRModel(RegressionModelApi):
         # init state
         self.__state = False
         # init params
-        self.__params = {'criterion': ['friedman_mse'],
-                         'max_depth': [10, 20],
-                         'max_leaf_nodes': [i for i in range(65, 105, 10)],
-                         'min_samples_split': [i for i in range(55, 105, 10)]}
+        self.__params = {
+            'n_estimators': [50],
+            'max_depth': [10, 20],
+            'min_samples_split': [55, 75],
+            'max_leaf_nodes': [80, 100]
+        }
         # init GridSearchCV based on cross-validator and params
-        self.__search = GridSearchCV(estimator=DecisionTreeRegressor(), param_grid=self.__params, cv=self.__cv,
+        self.__search = GridSearchCV(estimator=RandomForestRegressor(), param_grid=self.__params, cv=self.__cv,
                                      scoring='r2', verbose=10, n_jobs=-1)
         # init logs
         self.__report = dict()
@@ -51,7 +52,7 @@ class DTRModel(RegressionModelApi):
         # model params
         report.update(
             {
-                "model": "sklearn.linear_model.DecisionTreeRegressor",
+                "model": "sklearn.linear_model.RandomForestRegressor",
                 "best_estimator": f"{self.__search.best_estimator_}",
                 "best_params": f"{self.__search.best_params_}",
                 "R2 score": f"{self.__search.best_score_}"
@@ -69,7 +70,7 @@ class DTRModel(RegressionModelApi):
         self.__search.fit(X=x_train, y=y_train)
         print("TEST INFO")
         print(self.__search.best_params_)
-        print("-"*100)
+        print("-" * 100)
         print(self.__search.best_estimator_)
         print("-" * 100)
 
@@ -87,11 +88,11 @@ class DTRModel(RegressionModelApi):
         )
 
         plt.scatter(y_test, prediction)
-        plt.savefig(f"{path}/{self.__class__.__name__}-DTR1.png")
+        plt.savefig(f"{path}/{self.__class__.__name__}-RF1.png")
 
         fig, ax = plt.subplots()
         ax.scatter(y_test, y_test - prediction.reshape(prediction.shape[0], 1))
         ax.axhline(lw=2, color='black')
         ax.set_xlabel('Observed')
         ax.set_ylabel('Residual')
-        plt.savefig(f"{path}/{self.__class__.__name__}-DTR2.png")
+        plt.savefig(f"{path}/{self.__class__.__name__}-RF2.png")
