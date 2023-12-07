01a0dafc95d7d26bc29cfb96868b702bb3897967
astronely <nikborowik200394@gmail.com>
2023-12-07 23:34:32 +0500

    update: add decision tree implementation

--- labs/lab_3/gradient_boosting_regression/GradientBoostingRegressionModel.py
+++ labs/lab_3/gradient_boosting_regression/GradientBoostingRegressionModel.py
@@ -1,4 +1,6 @@
+import numpy as np
 from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import HistGradientBoostingRegressor
 
 from labs.lab_3.util.data.BaseRegressionModel import BaseRegressionModel
 
@@ -7,12 +9,12 @@ class GradientBoostingRegressionModel(BaseRegressionModel):
     def __init__(self):
         super().__init__(
             params={
-                'max_depth': [6],
-                'n_estimators': [3],
-                'learning_rate': [1.0],
-                'random_state': [42]
+                'max_depth': [5, 15, 20, 50],
+                'max_leaf_nodes': [10, 20, 30, 60],
+                'learning_rate': [.1, .12],
+                'random_state': [4242134]
             },
-            estimator=GradientBoostingRegressor(),
+            estimator=HistGradientBoostingRegressor(),
             name=f"{self.__class__.__name__}",
             load_graphics=True
         )
